395da16bf500b3f2f38492514f72b8bd8fe54dd4
Rutvik <81707575+code-rutvik@users.noreply.github.com>
2021-05-20 10:23:11 +0530

    Add files via upload

---
+++ calander.py
@@ -0,0 +1,92 @@
+# importing calendar module
+import calendar
+
+yy = 2000  # year
+mm = 1   # month
+mm = 2   # month
+mm = 3   # month
+mm = 4   # month
+mm = 5   # month
+mm = 6   # month
+mm = 7   # month
+mm = 8   # month
+mm = 9   # month
+mm = 10   # month
+mm = 11   # month
+mm = 12   # month
+
+yy = 2001  # year
+mm = 1     #month
+mm = 2   # month
+mm = 3   # month
+mm = 4   # month
+mm = 5   # month
+mm = 6   # month
+mm = 7   # month
+mm = 8   # month
+mm = 9   # month
+mm = 10   # month
+mm = 11   # month
+mm = 12   # month
+
+yy = 2002  # year
+mm = 1     #month
+mm = 2   # month
+mm = 3   # month
+mm = 4   # month
+mm = 5   # month
+mm = 6   # month
+mm = 7   # month
+mm = 8   # month
+mm = 9   # month
+mm = 10   # month
+mm = 11   # month
+mm = 12   # month
+
+yy = 2003  # year
+mm = 1     #month
+mm = 2   # month
+mm = 3   # month
+mm = 4   # month
+mm = 5   # month
+mm = 6   # month
+mm = 7   # month
+mm = 8   # month
+mm = 9   # month
+mm = 10   # month
+mm = 11   # month
+mm = 12   # month
+
+yy = 2004  # year
+mm = 1     #month
+mm = 2   # month
+mm = 3   # month
+mm = 4   # month
+mm = 5   # month
+mm = 6   # month
+mm = 7   # month
+mm = 8   # month
+mm = 9   # month
+mm = 10   # month
+mm = 11   # month
+mm = 12   # month
+
+yy = 2005  # year
+mm = 1     #month
+mm = 2   # month
+mm = 3   # month
+mm = 4   # month
+mm = 5   # month
+mm = 6   # month
+mm = 7   # month
+mm = 8   # month
+mm = 9   # month
+mm = 10   # month
+mm = 11   # month
+mm = 12   # month
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
